182a394249ae62282f8ec8a3e10d697472a3d18a
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-03 14:49:46 +0630

    agent link generate updated

--- resources/views/admin_layouts/sidebar.blade.php
+++ resources/views/admin_layouts/sidebar.blade.php
@@ -41,7 +41,7 @@
         <span class="sidenav-normal  ms-2  ps-1">Agent List</span>
       </a>
     </li>
-        @can('agent_index')
+   
     <li class="nav-item">
       <a class="nav-link text-white " href="{{ route('admin.random_agent_link')}}">
         <span class="sidenav-mini-icon"> <i class="fa-solid fa-user"></i> </span>
