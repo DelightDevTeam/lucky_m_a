42143b22948ee8619a6fcf62f9cfe27361bc8f75
mawite01 <thangneimawi@aceplussolutions.com>
2024-08-05 11:48:16 +0630

    fixed register

--- app/Http/Requests/Api/RegisterRequest.php
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
             'name' => 'required|min:3|string',
             'password' => 'required|min:6|confirmed',
             'phone' => 'required',
-            'referral_code' => ['required', 'exists:users, refrral_code']
+            'referral_code' =>'required|exists:users,referral_code',
         ];
     }
 }
