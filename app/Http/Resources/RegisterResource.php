9ed1290ddd36329bfb1bc4a657532fafe1374c5b
mawite01 <thangneimawi@aceplussolutions.com>
2024-08-05 20:49:29 +0630

    fixed register

--- app/Http/Resources/RegisterResource.php
+++ app/Http/Resources/RegisterResource.php
@@ -14,11 +14,18 @@ class RegisterResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        $user = [
             'id' => $this->id,
             'name' => $this->name,
-            'user_name' => $this->user_name,
-            'phone' => $this->phone
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'balance' => $this->balanceFloat,
+            'status' => $this->status,
+        ];
+
+        return [
+            'user' => $user,
+            'token' => $this->createToken($this->user_name)->plainTextToken,
         ];
     }
 }
