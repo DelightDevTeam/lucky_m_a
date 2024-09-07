6a7a80aef204e31d92b2db277abcbcde7ce2023c
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-09-07 09:12:26 +0630

    game integrate

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,13 +15,13 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         $admin = $this->createUser(UserType::Admin, 'Owner', 'luckym', '09123456789');
-        (new WalletService)->deposit($admin, 10 * 100_000, TransactionName::CapitalDeposit);
+        (new WalletService)->deposit($admin, 502000, TransactionName::CapitalDeposit);
 
         $agent_1 = $this->createUser(UserType::Agent, 'Agent 1', 'A898737', '09112345674', $admin->id, 'vH4HueE9');
-        (new WalletService)->transfer($admin, $agent_1, 5 * 100_000, TransactionName::CreditTransfer);
+        (new WalletService)->transfer($admin, $agent_1, 0.0, TransactionName::CreditTransfer);
 
         $player_1 = $this->createUser(UserType::Player, 'Player 1', 'P111111', '09111111111', $agent_1->id);
-        (new WalletService)->transfer($agent_1, $player_1, 30000, TransactionName::CreditTransfer);
+        (new WalletService)->transfer($agent_1, $player_1, 0.0, TransactionName::CreditTransfer);
     }
 
     private function createUser(UserType $type, $name, $user_name, $phone, $parent_id = null, $referral_code = null)
